9f7d0ec388dd1267abb149a569e542a66ee20d62
Batuhan Demir <batuhandemirp@gmail.com>
2020-03-07 10:00:45 +0300

    .

--- app/src/main/java/com/android/batdemir/library/ui/base/activity/BaseActivity.java
+++ app/src/main/java/com/android/batdemir/library/ui/base/activity/BaseActivity.java
@@ -8,14 +8,13 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.batdemir.library.R;
+import com.android.batdemir.library.ui.base.controller.BaseController;
+import com.android.batdemir.library.ui.base.factory.BindingFactory;
+import com.android.batdemir.library.ui.base.factory.ControllerFactory;
 import com.android.batdemir.mydialog.listeners.MyAlertDialogButtonListener;
 import com.android.batdemir.mydialog.ui.MyAlertDialog;
 import com.android.batdemir.mydialog.ui.MyDialogStyle;
-import com.batdemir.template.R;
-import com.batdemir.template.ui.activities.base.controller.BaseController;
-import com.batdemir.template.ui.activities.base.factory.BindingFactory;
-import com.batdemir.template.ui.activities.base.factory.ControllerFactory;
-import com.batdemir.utilities.MethodHelper;
 
 @SuppressWarnings({"squid:S00119"})
 public abstract class BaseActivity<Binding, Controller extends BaseController> extends AppCompatActivity implements
@@ -43,7 +42,6 @@ public abstract class BaseActivity<Binding, Controller extends BaseController> e
     protected void onResume() {
         super.onResume();
         runOnUiThread(() -> {
-            MethodHelper.getInstance().setScanner(this, false);
             loadData();
             setListeners();
         });
