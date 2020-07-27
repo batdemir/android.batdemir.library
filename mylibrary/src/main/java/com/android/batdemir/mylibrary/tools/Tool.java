3f54bb4f844e02db20f6fbea81027b91225ffe4a
Batuhan Demir <batuhandemirp@gmail.com>
2020-07-27 17:15:20 +0300

    Bintray Upload V2.3.4
    Added button visibility to progressbar

--- mylibrary/src/main/java/com/android/batdemir/mylibrary/tools/Tool.java
+++ mylibrary/src/main/java/com/android/batdemir/mylibrary/tools/Tool.java
@@ -7,10 +7,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import androidx.annotation.AnimRes;
 
 import com.android.batdemir.mylibrary.R;
+
+import java.util.Objects;
 
 public class Tool {
 
@@ -48,16 +54,43 @@ public class Tool {
                     activity.finish();
             }
         } catch (Exception e) {
-            Log.e(Tool.class.getSimpleName(), e.getMessage());
+            Log.e(Tool.class.getSimpleName(), Objects.requireNonNull(e.getMessage()));
         }
     }
 
-    public void anim(View view, int animId) {
+    public void move(Class<?> to, View view, boolean isLeft, boolean isHistory, Bundle bundle) {
+        try {
+            ProgressBar progressBar = new ProgressBar(view.getContext(), null, android.R.attr.progressBarStyle);
+            progressBar.setLayoutParams(view.getLayoutParams());
+            progressBar.setIndeterminate(true);
+            ViewGroup viewGroup = (ViewGroup) view.getParent();
+            viewGroup.addView(progressBar);
+            view.setVisibility(View.GONE);
+            Activity activity = (Activity) context;
+            Intent intent = new Intent(context, to);
+            if (bundle != null)
+                intent.putExtras(bundle);
+            context.startActivity(intent);
+            if (isLeft) {
+                activity.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                if (!isHistory)
+                    activity.finish();
+            } else {
+                activity.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                if (!isHistory)
+                    activity.finish();
+            }
+        } catch (Exception e) {
+            Log.e(Tool.class.getSimpleName(), Objects.requireNonNull(e.getMessage()));
+        }
+    }
+
+    public void anim(View view, @AnimRes int animId) {
         try {
             Animation animation = AnimationUtils.loadAnimation(context, animId == 0 ? R.anim.slide_down : animId);
             view.startAnimation(animation);
         } catch (Exception e) {
-            Log.e(Tool.class.getSimpleName(), e.getMessage());
+            Log.e(Tool.class.getSimpleName(), Objects.requireNonNull(e.getMessage()));
         }
     }
 }
